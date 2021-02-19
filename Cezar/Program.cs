47f567299f1e7bef46695d7a14040a22f69d9623
Шайхутдинова Ильвина Ильшатовна <201719@mck-ktits.ru>
2021-02-19 18:13:06 +0300

    first

---
+++ Cezar/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Cezar
+{
+    public class CaesarCipher
+    {
+        //символы русской азбуки
+        const string rusAlfabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        //const string number = "0123456789";
+        //const string engAlfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        private string CodeEncode(string text, int k)
+        {
+            //добавляем в алфавит маленькие буквы
+            var fullRusAlfabet = rusAlfabet + rusAlfabet.ToLower();
+            //var fullEngAlfabet = engAlfabet + engAlfabet.ToLower();
+            var letterQty = fullRusAlfabet.Length;
+            var retVal = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                var c = text[i];
+                var index = fullRusAlfabet.IndexOf(c);
+                if (index < 0)
+                {
+                    //если символ не найден, то добавляем его в неизменном виде
+                    retVal += c.ToString();
+                }
+                else
+                {
+                    var codeIndex = (letterQty + index + k) % letterQty;
+                    retVal += fullRusAlfabet[codeIndex];
+                }
+            }
+
+            return retVal;
+        }
+
+        //шифрование текста
+        public string Encrypt(string plainMessage, int key)
+            => CodeEncode(plainMessage, key);
+
+        //дешифрование текста
+        public string Decrypt(string encryptedMessage, int key)
+            => CodeEncode(encryptedMessage, -key);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cipher = new CaesarCipher();
+            Console.Write("Введите текст: ");
+            var message = Console.ReadLine();
+            Console.Write("Введите ключ: ");
+            var secretKey = Convert.ToInt32(Console.ReadLine());
+            var encryptedText = cipher.Encrypt(message, secretKey);
+            Console.WriteLine("Зашифрованное сообщение: {0}", encryptedText);
+            Console.WriteLine("Расшифрованное сообщение: {0}", cipher.Decrypt(encryptedText, secretKey));
+            Console.ReadLine();
+        }
+    }
+}
+
+
